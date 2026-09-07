7b85193b7f9dbd132bcb63b363cc1e165fc7b3b1
Fabián David Vargas Quijano <fvargas@nexura.com>
2026-09-07 15:48:51 -0500

    'feat(reportes):

--- build-mfe.js
+++ build-mfe.js
@@ -81,7 +81,19 @@ async function buildMfe() {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="http://local-cdn.nexura.com.co/web-components/admin/v1/layout.esm.js" type="module"></script>
+    <script>
+      (() => {
+        const isPreprod = location.href.includes('https://preproduccion9-jcc.nexura.com.co');
+        const cdnUrl = isPreprod
+          ? 'https://preproduccion9-jcc.nexura.com.co/mod/Galeria/js/web-components/admin/v1/layout.esm.js'
+          : 'http://local-cdn.nexura.com.co/web-components/admin/v1/layout.esm.js';
+
+        const script = document.createElement('script');
+        script.type = 'module';
+        script.src = cdnUrl;
+        document.head.appendChild(script);
+      })();
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
   </head>
   <body>
